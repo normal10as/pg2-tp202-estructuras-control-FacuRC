dc2b50077a5064fe5faad3fc95c67444fbba49f8
Facundo Cardozoo <cardozo_facundo_95@hotmail.com>
2018-06-25 11:37:24 -0300

    2.15

--- Estructuras_Control_Flujo/Estructuras_Control_Flujo/2_14.vb
+++ Estructuras_Control_Flujo/Estructuras_Control_Flujo/2_14.vb
@@ -5,37 +5,47 @@
 	'cada producto subtotal (cantidad por precio unitario), porcentaje de descuento aplicado, monto
 	'descontado y el total (subtotal – monto descontado). Al finalizar el ingreso, por cantidad igual a
 	'cero, informar el total de pesos descontados y el total a cobrar.
+
 	Sub main()
-		Dim cantidad, PrecioUnitario, subtotal, MontoDescontado As Decimal
+		Dim cantidad, PrecioUnitario, subtotal, MontoDescontado, total, tpd, tc As Decimal
 		Dim descuento As Byte
-		cantidad = 1
-		Do While cantidad > 0
-			Console.Write("Ingrese cantidad:")
+
+		Do
+			Console.Write("Ingrese cantidad  :")
 			cantidad = Console.ReadLine
-			cantidad += cantidad
+
 			Console.Write("Ingrese Precio Unitario: ")
 			PrecioUnitario = Console.ReadLine
-			PrecioUnitario += PrecioUnitario
-		Loop
 
-		If cantidad >= 10 And cantidad <= 50 Then
-			descuento = 5
-		ElseIf cantidad > 50 And cantidad <= 250 Then
-			descuento = 10
-		ElseIf cantidad > 250 Then
-			descuento = 20
-		Else
-			descuento = 0
-		End If
+			If cantidad >= 10 And cantidad <= 50 Then
+				descuento = 5
+			ElseIf cantidad > 50 And cantidad <= 250 Then
+				descuento = 10
+			ElseIf cantidad > 250 Then
+				descuento = 20
+			Else
+				descuento = 0
+			End If
 
-		subtotal = cantidad * PrecioUnitario
-		MontoDescontado = subtotal * descuento / 100
+			subtotal = cantidad * PrecioUnitario
+			MontoDescontado = subtotal * descuento / 100
+			total = subtotal - MontoDescontado
 
-		Console.WriteLine("Subtotal:" & subtotal)
-		Console.WriteLine("Porcentaje Descontado: " & descuento & "%")
-		Console.WriteLine("Total de pesos Descontados: " & MontoDescontado)
-		Console.WriteLine("Total a cobrar: " & subtotal - MontoDescontado)
 
+			Console.WriteLine("subtotal: $ " & subtotal)
+			Console.WriteLine("Descuento aplicado: {0} %", descuento)
+			Console.WriteLine("Monto Descontado: $ " & MontoDescontado)
+			Console.WriteLine("Total: $ " & total)
+			Console.WriteLine(" ************************************* ")
+
+			tpd = tpd + MontoDescontado
+			tc = tc + total
+
+		Loop While cantidad <> 0
+		Console.WriteLine("-------------------------------------------")
+		Console.WriteLine("Total de pesos descontados: $ " & tpd)
+		Console.WriteLine("Total a cobrar: $ " & tc)
+		Console.WriteLine("-------------------------------------------")
 		Console.ReadKey()
 
 	End Sub
---
+++ Estructuras_Control_Flujo/Estructuras_Control_Flujo/2_15.vb
@@ -0,0 +1,43 @@
+﻿Module _2_15
+	'	2.15. Dado el siguiente planteo: se deposita un capital a un plazo de n meses, con una tasa de
+	'interés de 36% anual (es una constante), capitalizable cada mes, es decir, se suman los intereses
+	'ganados al capital. Elaborar un algoritmo que ingrese el capital inicial, la cantidad de meses y
+	'luego imprima el siguiente reporte
+	'Donde:
+	'MES: es el número de mes.
+	'CAPITAL: en el primer mes es el capital inicial y en los meses siguientes es el saldo
+	'acumulado anterior. Por ejemplo, en el mes 1, el capital es el ingresado, para el mes 2, deberá
+	'tener incrementado el interés que ganó en el mes 1, y así sucesivamente.
+	'INTERES: es el capital por la tasa mensual de interés (tasa anual / 12), osea es el valor que
+	'gana en el mes correspondiente.
+	'SALDO: es el el capital sumado el interés.
+	'TOTAL INTERES GANADO: es la sumatoria de los intereses mensuales.
+	Sub main()
+		Dim Capital, totalInteres, interes As Decimal
+		Dim InteresMensual As Decimal = 0.03
+		Dim mes As Byte
+
+		Console.Write("Ingrese capital Inicial: ")
+		Capital = Console.ReadLine
+
+		Console.Write("Ingrese cantidad de meses: ")
+		mes = Console.ReadLine
+
+		Console.WriteLine(vbCrLf & "{0, 4} {1, 15} {2, 15} {3, 15}", "MES", "CAPITAL", "INTERES", "SALDO")
+
+		For n = 1 To mes
+			interes = Capital * InteresMensual
+			totalInteres += interes
+			Console.WriteLine("{0, 4} {1, 15} {2, 15} {3, 15}", Math.Round(n, 2), Math.Round(Capital, 2), Math.Round(interes, 2), Math.Round(Capital + interes, 2))
+			Capital += interes
+		Next
+
+		Console.WriteLine(vbCrLf & "TOTAL INTERES GANADO:" & Math.Round(totalInteres, 2))
+
+		Console.ReadKey()
+
+
+
+
+	End Sub
+End Module
